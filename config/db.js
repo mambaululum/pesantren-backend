aae863ba1344dad371f302ab2951b390fd35d0e1
mambaululum <mambaululum00@gmail.com>
2026-05-25 20:17:04 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+console.log('DATABASE_URL:', process.env.DATABASE_URL ? 'ADA' : 'TIDAK ADA');
+
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
