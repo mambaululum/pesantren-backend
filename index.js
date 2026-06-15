b6644362f722eb97d77615d9e29008bc0c70cf11
Mambaul Ulum <mambaululum00@gmail.com>
2026-06-15 23:13:40 +0700

    fix: file index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],   // tambahkan 'PATCH'
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json({ limit: '10mb' }));
